92ccd81ec033997d105b43a8e352440c62b0ace0
shantellwilliams12 <shantell.williams0312@gmail.com>
2021-07-28 18:25:57 -0400

    Get current date

--- ContactLibrary/Program.cs
+++ ContactLibrary/Program.cs
@@ -6,7 +6,64 @@ namespace ContactLibrary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Your PhoneBook App");
+            Console.WriteLine("Select operation");
+            Console.WriteLine("1) Add Contact");
+            Console.WriteLine("2) Display contact by number");
+            Console.WriteLine("3) View all contacts");
+            Console.WriteLine("4) Search contacts by name");
+            Console.WriteLine("Press 'x' to exit");
+
+            var userInput = Console.ReadLine();
+
+            var phoneBook = new PhoneBook();
+
+            while (true)
+            {
+                switch (userInput)
+                {
+                    case "1":
+                        Console.WriteLine("Contact name: ");
+                        var name = Console.ReadLine();
+                        Console.WriteLine("Contact number: ");
+                        var number = Console.ReadLine();
+
+                        var newContact = new Contact(name, number);
+                        phoneBook.AddContact(newContact);
+
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Contact number: ");
+                        var searchNumber = Console.ReadLine();
+                        phoneBook.DisplayContact(searchNumber);
+
+                        break;
+
+                    case "3":
+                        phoneBook.DisplayAllContact();
+
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Name search phrase");
+                        var searchPhrase = Console.ReadLine();
+
+                        phoneBook.DisplayMatchingContacts(searchPhrase);
+
+                        break;
+
+                    case "x":
+                        return;
+
+                    default:
+                        Console.WriteLine("Please select valid operation");
+                        break;
+                }
+
+                Console.WriteLine("Select operation");
+                userInput = Console.ReadLine();
+            }
         }
     }
 }
