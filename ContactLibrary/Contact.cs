5b7283724b9de9a00084e3b48c55ba3889953e7a
shantellwilliams12 <shantell.williams0312@gmail.com>
2021-11-17 19:11:54 -0500

    Add Email Option

--- ContactLibrary/Contact.cs
+++ ContactLibrary/Contact.cs
@@ -8,14 +8,16 @@ namespace ContactLibrary
     class Contact
     {
 
-        public Contact(string name, string number)
+        public Contact(string name, string number, string email)
         {
             Name = name;
             Number = number;
+            Email = email;
         }
 
         public string Name { get; set; }
         public string Number { get; set; }
+        public string Email { get; set; }
 
     }
 }
